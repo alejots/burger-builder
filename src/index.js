ca5b367c560b9903c5a97ff58a78f5fa400a885e
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2019-08-05 18:13:23 +1200

    Hide redux devtools for production

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ import BurgerBuilderReducer from "./BurgerBuilder/BurgerBuilderReducer";
 import OrdersReducer from "./Orders/OrdersReducer";
 import AuthReducer from "./Auth/AuthReducer";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: BurgerBuilderReducer,
